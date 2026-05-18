044221cef9dfce15402306bed21433df4ef38aeb
Pradeep Ahirwar <pradeepahirwarjul92@gmail.com>
2026-05-18 17:37:29 +0530

    added

--- src/main/java/com/heg/controller/TestController.java
+++ src/main/java/com/heg/controller/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "CI/CD Using Jenkinsfile in Project ";
+		return "CI/CD Using Jenkinsfile in Project -------- check ";
 	}
 
 }
