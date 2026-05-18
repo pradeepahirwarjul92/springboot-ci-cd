83b875c746eb298224a0c1f9b08d98f6433b9bfd
Pradeep Ahirwar <pradeepahirwarjul92@gmail.com>
2026-05-18 16:51:32 +0530

    Pulling added here

--- src/main/java/com/heg/controller/TestController.java
+++ src/main/java/com/heg/controller/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "CI/CD Working \n New Changes added";
+		return "CI/CD Working \n New Changes added /n Polling checking ";
 	}
 
 }
